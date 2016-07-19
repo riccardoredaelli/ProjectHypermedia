cc69c5f66295a87035032a358ff1d2a82f51adf5
Riccardo Redaelli <riccardo.redaelli94@hotmail.it>
2016-07-19 10:04:33 +0200

    database

--- includes/js/assistanceServices.js
+++ includes/js/assistanceServices.js
@@ -4,7 +4,7 @@ function ready(){
     $.ajax({
         method: "POST",
         crossDomain: true, //localhost purposes
-        url: "includes/php/query.php", //percorso file.php
+        url: "http://hyp2016.altervista.org/includes/php/query.php", //percorso file.php
         data: {query : "SELECT * FROM categoriaservizioassistenza ORDER BY idservizio ASC"},
         success: function(response) {
     
