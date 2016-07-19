0c37d2fa70fac1a1a75a23bf37827f56d4fc7924
Riccardo Redaelli <riccardo.redaelli94@hotmail.it>
2016-07-19 11:37:09 +0200

    final

--- includes/php/query.php
+++ includes/php/query.php
@@ -4,14 +4,6 @@ header("Access-Control-Allow-Origin: *");
 define('MYSQL_NUM',MYSQLI_NUM);
 define('MYSQL_ASSOC',MYSQLI_ASSOC);*/
 
-/*$server = "localhost";
-$username = "hyp2016@localhost";
-$password = "";
-$database = "my_hyp2016";
-
-$con = mysql_connect($server, $username, $password) or die ("Could not connect: " . mysql_error());
-mysql_select_db($database, $con);*/
-
 $mysqli = new mysqli("localhost", "root", "", "my_hyp2016");
 
 $query=$_POST["query"];
